7a1142090736b0bc9a5e5a51d2d16d6dccf6cd3c
christyjlewis <43048648+christyjlewis@users.noreply.github.com>
2024-07-26 17:06:05 -0600

    moving to new directory for packaging

---
+++ climate_trace_tools/compare/subtract_out/helper.py
@@ -0,0 +1,36 @@
+import json
+from climate_trace_tools.compare.subtract_out.util.country_lists import (
+    countries_annex1,
+    countries_nonannex1,
+)
+
+
+class InputHelper:
+    def __init__(self):
+        with open("files/master_comparison_dict_annex1.json", "r") as f:
+            self.master_comparison_dict_annex1 = json.loads(f.read())
+
+        with open("files/master_comparison_dict_nonannex1.json", "r") as f:
+            self.master_comparison_dict_nonannex1 = json.loads(f.read())
+
+    def get_available_countries(self, annex1):
+        if annex1:
+            return countries_annex1
+        else:
+            return countries_nonannex1
+
+    def sectors_available_to_plot(self, annex1):
+        if annex1:
+            compdict = self.master_comparison_dict_annex1
+        else:
+            compdict = self.master_comparison_dict_nonannex1
+
+        return list(compdict.keys())
+
+    def inventories_available_to_compare_for_sector(self, sector, annex1):
+        if annex1:
+            compdict = self.master_comparison_dict_annex1
+        else:
+            compdict = self.master_comparison_dict_nonannex1
+
+        return list(compdict[sector].keys())
