39a5d246e7399c66b9d49b58eeaaed72cfead290
christyjlewis <43048648+christyjlewis@users.noreply.github.com>
2024-07-31 10:32:05 -0600

    removing os from requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
 setup(
-    name = "climate_trace_tools",
-    version = '0.0.1',
-    description = 'tools for comparing climate trace data to other data sources',
-    author = 'Christy Lewis',
-    author_email = 'christy@watttime.org',
-    install_requires = [
-        'numpy',
-        'pandas',
-        'os',
-        'plotly',
-        'json',
-        'copy',
-        ],
-    include_package_data=True
-    )
+    name="climate_trace_tools",
+    version="0.0.1",
+    description="tools for comparing climate trace data to other data sources",
+    author="Christy Lewis",
+    author_email="christy@watttime.org",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "plotly",
+        "json",
+        "copy",
+    ],
+    include_package_data=True,
+)
