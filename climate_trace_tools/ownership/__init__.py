e64bf1321eae21ea491cfb49a0f6db0092554004
Amy Kouch <amy.kouch@watttime.org>
2026-02-13 13:47:09 -0800

    added ownership and assetfinder

---
+++ climate_trace_tools/ownership/__init__.py
@@ -0,0 +1,1 @@
+from .ownership import find_owner_sources, find_owners_emissions
--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ setup(
         "plotly",
         "openpyxl",
         "psycopg2",
+        "shapely",
+        "geopy",
+        "google-cloud-bigquery",
+        "requests",
     ],
     packages=find_packages(),  # This ensures that all packages and sub-packages are included
     include_package_data=True,  # This tells setuptools to include files specified in MANIFEST.in or package_data
@@ -19,6 +23,7 @@ setup(
         "climate_trace_tools": [
             "data/country/*.zip",
             "data/source/*.zip",
+            "data/ownership/*.zip",
             "data/supplementary/*.csv",
             "compare/subtract_out/files/*.csv",
             "compare/subtract_out/files/*.xlsx",
