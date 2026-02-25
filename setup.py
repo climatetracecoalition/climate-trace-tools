c956dc926e8fe5b6d93425ad86d33ed428465bb8
Amy Kouch <amy.kouch@watttime.org>
2026-02-25 13:59:19 -0800

    updated setup file

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
         "geopy",
         "google-cloud-bigquery",
         "requests",
+        "networkx",
+        "tqdm",
     ],
     packages=find_packages(),  # This ensures that all packages and sub-packages are included
     include_package_data=True,  # This tells setuptools to include files specified in MANIFEST.in or package_data
